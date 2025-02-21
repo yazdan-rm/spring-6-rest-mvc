405eda473105786b55ab05c6f7b3eb38f1a96983
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-21 19:46:07 +0330

    List Beeer

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -5,18 +5,32 @@ import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
     private final BeerService beerService;
 
-    public Beer getBeerById(UUID id) {
+    @GetMapping
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
+
+    @GetMapping("/{beerId}")
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
         log.debug("Get Beer Id - in controller");
-        return beerService.getBeerById(id);
+        Beer beerById = beerService.getBeerById(beerId);
+        System.out.println(beerById);
+        return beerById;
     }
 }
--- src/main/java/guru/springframework/spring6restmvc/model/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/model/Beer.java
@@ -17,6 +17,6 @@ public class Beer {
     private String upc;
     private Integer quantityOnHand;
     private BigDecimal price;
-    private LocalDateTime createDate;
+    private LocalDateTime createdDate;
     private LocalDateTime updateDate;
 }
