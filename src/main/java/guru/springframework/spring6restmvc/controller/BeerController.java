b445abf986c2be581bd13627a220a9d4b2473570
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-03-01 01:52:18 +0330

    add devtools dependency to pom file

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -27,7 +27,7 @@ public class BeerController {
 
     @GetMapping("{beerId}")
     public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
-        log.debug("Get Beer Id - in controller");
+        log.debug("Get Beer Id - in controller -123");
         Beer beerById = beerService.getBeerById(beerId);
         System.out.println(beerById);
         return beerById;
