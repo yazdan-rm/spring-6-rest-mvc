b42e3934bb227a6b6a8df5da4f0b3fd6b1f2538f
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-28 11:26:08 +0330

    get by id

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,6 +1,5 @@
 package guru.springframework.spring6restmvc.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
