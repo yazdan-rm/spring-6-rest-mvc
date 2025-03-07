7a8aa6e17a04bba59eda1cd45154071e817904b4
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-03-07 20:13:13 +0330

    add http header to the response

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -4,10 +4,12 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,7 +24,12 @@ public class BeerController {
     @PostMapping
     public ResponseEntity<Beer> handlePost(@RequestBody Beer beer){
         Beer savedBeer = beerService.saveNewBeer(beer);
-        return new ResponseEntity<>(savedBeer, HttpStatus.CREATED);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.LOCATION, "/api/v1/beer/" + savedBeer.getId().toString());
+        headers.add("ZoneID", HttpHeaders.encodeBasicAuth("test", "test123", Charset.defaultCharset()));
+
+        return new ResponseEntity<>(savedBeer, headers, HttpStatus.CREATED);
     }
 
     @GetMapping
