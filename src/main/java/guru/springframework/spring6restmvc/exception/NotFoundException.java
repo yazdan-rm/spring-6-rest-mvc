47d87a51e485015622a30ce7f33450691b9cc236
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-04-01 13:50:26 +0330

    @ResponseStatus

--- src/main/java/guru/springframework/spring6restmvc/exception/NotFoundException.java
+++ src/main/java/guru/springframework/spring6restmvc/exception/NotFoundException.java
@@ -1,5 +1,9 @@
 package guru.springframework.spring6restmvc.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value not found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
         super();
