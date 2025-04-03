c91a390793aab6d143e1ebe0acf37560922fa74b
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-04-03 15:24:47 +0330

    create entity

---
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -0,0 +1,33 @@
+package guru.springframework.spring6restmvc.entities;
+
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Getter
+@Setter
+public class Beer {
+
+    @Id
+    private UUID id;
+
+    @Version
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
