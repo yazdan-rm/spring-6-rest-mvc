808237b79e6f47d4f8fcb978ec969c816f8adfaf
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-04-05 00:21:54 +0330

    add mapstruct to project

---
+++ src/main/java/guru/springframework/spring6restmvc/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6restmvc.mappers;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    Beer beerDtoToBeer(BeerDTO dto);
+
+    BeerDTO beerToBeerDTO(Beer beer);
+}
