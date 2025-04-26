704a341fa3f4b6881bb41923bf7c412faf98550b
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-04-27 01:20:09 +0330

    jpa customer get operation.

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
@@ -20,17 +21,17 @@ public class CustomerServiceJPA implements CustomerService {
 
     @Override
     public List<CustomerDTO> listCustomers() {
-        return List.of();
+        return customerRepository.findAll().stream().map(customerMapper::customerToCustomerDTO).collect(Collectors.toList());
     }
 
     @Override
     public Optional<CustomerDTO> getCustomerById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(customerMapper.customerToCustomerDTO(customerRepository.findById(id).orElse(null)));
     }
 
     @Override
     public CustomerDTO saveCustomer(CustomerDTO customer) {
-        return null;
+        return customerMapper.customerToCustomerDTO(customerRepository.save(customerMapper.customerDtoToCustomer(customer)));
     }
 
     @Override
---
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
@@ -0,0 +1,80 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.exception.NotFoundException;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
+import guru.springframework.spring6restmvc.repository.CustomerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class CustomerControllerIT {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CustomerController customerController;
+
+    @Test
+    void testGetCustomerById(){
+        Customer customer = customerRepository.findAll().getFirst();
+
+        CustomerDTO customerDTO = customerController.getCustomerById(customer.getId());
+
+        assertNotNull(customerDTO);
+
+    }
+
+    @Test
+    void testListCustomer(){
+        List<CustomerDTO> customers = customerController.getCustomers();
+        assertThat(customers.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyListCustomers(){
+        customerRepository.deleteAll();
+        List<CustomerDTO> customerDTOS = customerController.getCustomers();
+        assertThat(customerDTOS.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testCustomerIdNotFound(){
+        assertThrows(NotFoundException.class, () -> customerController.getCustomerById(UUID.randomUUID()));
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void saveCustomer(){
+        CustomerDTO customerDTO = CustomerDTO.builder()
+                .customerName("TEST YAZDAN")
+                .build();
+
+        ResponseEntity<CustomerDTO> responseEntity = customerController.handlePostRequest(customerDTO);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(201));
+        assertThat(responseEntity.getHeaders()).isNotNull();
+
+        String[] locationUUID = responseEntity.getHeaders().getLocation().getPath().split("/");
+        UUID saveUUID = UUID.fromString(locationUUID[locationUUID.length-1]);
+
+        Customer customer = customerRepository.findById(saveUUID).get();
+        assertThat(customer.getId()).isNotNull();
+    }
+}
