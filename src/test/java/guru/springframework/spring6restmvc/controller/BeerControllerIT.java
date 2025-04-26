4d105dc0f58be11d654a680482724219ac66a37c
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-04-26 22:26:21 +0330

    controller integration test

---
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -0,0 +1,38 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.model.BeerDTO;
+import guru.springframework.spring6restmvc.repository.BeerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> beers = beerController.listBeers();
+        assertThat(beers.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyListBeers() {
+        beerRepository.deleteAll();
+        List<BeerDTO> beers = beerController.listBeers();
+        assertThat(beers.size()).isEqualTo(0);
+    }
+}
